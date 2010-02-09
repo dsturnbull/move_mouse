affd7115d153ebb07aed02d70c61e97a05bbfba3
Dave Turnbull <dave@redcordial.org>
2010-02-09 22:53:35 +1100

    working against synergy

--- Makefile
+++ Makefile
@@ -1,3 +1,2 @@
 move_mouse: move_mouse.m
-	gcc -o move_mouse move_mouse.m -F/System/Library/Frameworks/Carbon.framework/Frameworks/ \
-		-framework ApplicationServices -framework Foundation -framework Carbon -framework Carbon 
+	gcc -o move_mouse move_mouse.m -F/System/Library/Frameworks/Carbon.framework/Frameworks/ -framework Foundation -framework Carbon
--- move_mouse.m
+++ move_mouse.m
@@ -1,30 +1,16 @@
 #import <Foundation/Foundation.h>
-#import <ApplicationServices/ApplicationServices.h>
 #import <HIToolbox/CarbonEvents.h>
 #include <stdio.h>
 
-void move_mouse(int x, int y);
 void register_hotkey(void);
 OSStatus hotkey_handler(EventHandlerCallRef next_handler, EventRef event, void *user_data);
 
-int x, y;
-
 int main(int argc, char *argv[]) {
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-  NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
+  // NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
 
-  x = [args integerForKey:@"x"];
-  y = [args integerForKey:@"y"];
-
-  // move_mouse(x, y);
   register_hotkey();
 
-  // NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
-  // while (true && [runLoop runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]]);
-
-  SetMouseCoalescingEnabled(false, NULL);
-  CGSetLocalEventsSuppressionInterval(0.0001);
-  
   RunApplicationEventLoop();
 
   [pool release];
@@ -38,30 +24,43 @@ void register_hotkey() {
 
   event_type.eventKind   = kEventHotKeyPressed;
   event_type.eventClass  = kEventClassKeyboard;
-  hotkey_id.signature    = 'htk1';
-  hotkey_id.id           = 1;
 
-  RegisterEventHotKey(49, optionKey, hotkey_id, GetApplicationEventTarget(), 0, &hotkey);
+  hotkey_id.signature    = 'left';
+  hotkey_id.id           = 0;
+  RegisterEventHotKey(123, cmdKey + shiftKey, hotkey_id, GetApplicationEventTarget(), 0, &hotkey);
+
+  hotkey_id.signature    = 'rght';
+  hotkey_id.id           = 1;
+  RegisterEventHotKey(124, cmdKey + shiftKey, hotkey_id, GetApplicationEventTarget(), 0, &hotkey);
+
   InstallApplicationEventHandler(&hotkey_handler, 1, &event_type, NULL, NULL);
 }
 
 OSStatus hotkey_handler(EventHandlerCallRef next_handler, EventRef event, void *user_data) {
-  int i;
+  int x, y;
 
-  CGEventRef ev  = CGEventCreate(NULL);
-  CGPoint cur_pt = CGEventGetLocation(ev);
-  NSLog(@"Location: %f, %f", (float)cur_pt.x, (float)cur_pt.y);
+  EventHotKeyID hotkey;
+  GetEventParameter(event, kEventParamDirectObject, typeEventHotKeyID, NULL, sizeof(hotkey), NULL, &hotkey);
 
-  for (i = 0; i < 5; i++)
-    move_mouse(x + i, y);
+  // 0 for left, 1 for right
+  if (!hotkey.id) {
+    x = -2000;
+    y = 500;
+  } else {
+    x = 2000;
+    y = 500;
+  }
 
-  return noErr;
-}
-
-void move_mouse(int x, int y) {
   NSLog(@"moving to %d, %d", x, y);
   CGPoint pt;
   pt.x = x;
   pt.y = y;
-  CGWarpMouseCursorPosition(pt);
+
+  CGEventRef ev = CGEventCreateMouseEvent(NULL, kCGEventMouseMoved, pt, kCGMouseButtonLeft);
+  CGEventSetType(ev, kCGEventMouseMoved);
+  CGEventPost(kCGHIDEventTap, ev);
+  CFRelease(ev);
+
+  return noErr;
 }
+
